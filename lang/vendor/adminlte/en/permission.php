39503d5e4f161df3a1bc6335a61d0c324f1fb3ac
amirul <hakim_s09@hotmail.com>
2023-06-08 19:11:54 +0800

    update permission lang

--- lang/vendor/adminlte/en/permission.php
+++ lang/vendor/adminlte/en/permission.php
@@ -3,4 +3,17 @@
 return [
 
     'import' => 'Import',
+    'profiles'                      => 'Profiles',
+    'socials'                       => 'Socials',
+    'educations'                    => 'Educations',
+    'languages'                     => 'Languages',
+    'interests'                     => 'Interests',
+    'experiences'                   => 'Experiences',
+    'projects'                      => 'Projects',
+    'skills'                        => 'Skills',
+    'icons'                         => 'Icons',
+    'educationlevel'                => 'Educations Level',
+    'proficiencylevel'              => 'Proficiency Level',
+    'statuslist'                    => 'Status',
+    'attachments'                   => 'Attachments',
 ];
